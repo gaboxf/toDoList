db08a1242fdd742d9eaa0389ef5fab2f3c6a3475
gaboxf <45081087+gaboxf@users.noreply.github.com>
2021-09-05 18:05:39 -0300

    Primera version de la lista de tareas

---
+++ v0/js/main.js
@@ -0,0 +1,29 @@
+const addBtn = document.getElementById("addBtn"); //elemendo del dom que tiene a addbtn
+addBtn.addEventListener("click", addTask); //se le agrega el evento y al finalizar se ejecuta la funcion
+
+function addTask(){
+    const taskName = document.getElementById("taskName").value;
+    const nodeTask = document.createElement("li");
+    nodeTask.innerHTML = 
+                    `
+                    <div>
+                        <input type="checkbox" name="" id="">
+                        <span>${taskName}</span>
+                    </div>
+                    <input type="button" value="X" onclick="this.parentElement.remove()">
+                    `;
+    const list = document.getElementById("list");
+    list.prepend(nodeTask);//agrega un hijo al principio.
+
+    clearInput();
+    /*  OPCIONES    */
+    //const list = document.querySelector("#list");
+    //list.appendChild(task); //agrega un hijo al final.
+    //list.insertBefore(nodeTask, list.childNodes[0]);
+    //list.insertAdjacentElement("beforebegin", task);
+}
+
+function clearInput(){
+    let taskName = document.getElementById("taskName");
+    taskName.value = "";
+}
